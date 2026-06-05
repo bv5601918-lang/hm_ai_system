9ef6a49df8f4f35679a79edbf16041a95daf398a
bv5601918-lang <bv5601918@gmail.com>
2026-06-05 12:43:07 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let balance = 0;
+
 function showPage(page) {
 
   document.getElementById("home").style.display = "none";
@@ -6,4 +8,13 @@ function showPage(page) {
   document.getElementById("menu").style.display = "none";
 
   document.getElementById(page).style.display = "block";
+}
+
+function watchAd() {
+  balance += 0.50;
+
+  document.getElementById("balanceText").innerText =
+    "💰 Balance: ৳" + balance.toFixed(2);
+
+  alert("🎉 You earned ৳0.50");
 }
