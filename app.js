f0496017d6cfeb63b4394aa8801c18c6b7d89801
bv5601918-lang <bv5601918@gmail.com>
2026-06-05 14:23:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-let balance = 0;
-
-function showPage(page) {
-
-  document.getElementById("home").style.display = "none";
-  document.getElementById("tasks").style.display = "none";
-  document.getElementById("packages").style.display = "none";
-  document.getElementById("menu").style.display = "none";
-
-  document.getElementById(page).style.display = "block";
-}
-
-function watchAd() {
-  balance += 0.50;
+function submitTask(amount) {
+  balance += amount;
 
   document.getElementById("balanceText").innerText =
     "💰 Balance: ৳" + balance.toFixed(2);
 
-  alert("🎉 You earned ৳0.50");
+  alert("✅ Task Submitted! +৳" + amount);
 }
